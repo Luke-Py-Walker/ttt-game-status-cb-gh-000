f1d8b99cffdba16789cd61d71e8a3b3d261211d7
Lucas Wang <lucas211w@gmail.com>
2018-07-05 22:23:55 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,9 +8,9 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5
 
 def won?(board)
   WIN_COMBINATIONS.detect do |win_index|
-    position_1 = 1 #board[win_index[0]]
-    position_2 = board[win_index[1]]
-    position_3 = board[win_index[2]]
+    position_1 = position_taken?(board,win_index[0]) ? board[win_index[0]] : 1
+    position_2 = position_taken?(board,win_index[1]) ? board[win_index[0]] : 3
+    position_3 = position_taken?(board,win_index[2]) ? board[win_index[0]] : 1
     position_1 == position_2 && position_2 == position_3
   end
 end
